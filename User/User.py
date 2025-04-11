377f5f0626c0a3e6fbf2c644d42ec34375e8d7d5
kusbah <abdkusbah2@gmail.com>
2025-04-11 16:51:03 +0200

    complete Chaining Methods

--- User/User.py
+++ User/User.py
@@ -5,29 +5,34 @@ class User:
         self.balance = balance
     def make_deposit(self, amount): 
         self.balance += amount
+        return self
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
     def display_user_balance(self):
         print(f"Name: {self.name}\n Balance: {self.balance}")
+        return self
     def transfer_money(self, other_user, amount):
         self.balance -= amount
         other_user.balance += amount
+        return self
 
 user1 = User("abd","abd@gmial.com")
 user2 = User("khaild","khaild@gmial.com")
 user3 = User("ahmad","ahmad@gmial.com")
 
-user1.make_deposit(100)
-user1.make_deposit(100)
-user1.make_deposit(100)
-user1.make_withdrawal(100)
-user1.display_user_balance()
-
-user2.make_deposit(200)
-user2.make_deposit(100)
-user2.make_withdrawal(100)
-user2.make_withdrawal(100)
-user2.display_user_balance()
+user1.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_user_balance()
+# user1.make_deposit(100)
+# user1.make_deposit(100)
+# user1.make_deposit(100)
+# user1.make_withdrawal(100)
+# user1.display_user_balance()
+user2.make_deposit(200).make_deposit(100).make_withdrawal(100).make_withdrawal(50).display_user_balance
+# user2.make_deposit(200)
+# user2.make_deposit(100)
+# user2.make_withdrawal(100)
+# user2.make_withdrawal(100)
+# user2.display_user_balance()
 
 user1.transfer_money(user2,50)
 user1.display_user_balance()
