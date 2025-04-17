b15675597e8b5e559b9b3a0dd1c575e2438601e8
kusbah <abdkusbah2@gmail.com>
2025-04-17 19:09:26 +0200

    complete MathDojo + TDD(Optional)

--- MathDojo/MathDojo.py
+++ MathDojo/MathDojo.py
@@ -1,3 +1,4 @@
+import unittest
 class MathDojo:
     def __init__(self):
         self.result = 0
@@ -18,3 +19,16 @@ md = MathDojo()
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x)    # should print 5
 # run each of the methods a few more times and check the result!
+
+class TestMathDojo(unittest.TestCase):
+    def setUp(self):
+        self.md = MathDojo()
+    def test_MathDojo(self):
+        self.md.add(5)
+        self.assertEqual(self.md.result,5)
+    def test_add_subtract(self):
+        self.md.add(2).add(2,5,1).subtract(3,2)
+        self.assertEqual(self.md.result,5)
+
+if __name__ == '__main__':
+    unittest.main()
