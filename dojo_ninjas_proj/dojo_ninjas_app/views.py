f2bbbee328796b68c580927d2c16a7d78952f9c8
kusbah <abdkusbah2@gmail.com>
2025-06-15 18:15:48 +0300

    complete Dojos & Ninjas with Template

--- dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello World!")
+    if request.method == 'POST':
+        Dojo.objects.create(
+            name = request.POST.get('name'),
+            city = request.POST.get('city'),
+            state = request.POST.get('state')
+        )
+        # dojo_id = request.POST.get('dojo')
+        # dojo = Dojo.objects.get(id=dojo_id)
+        Ninja.objects.create(
+            first_name = request.POST.get('first_name'),
+            last_name = request.POST.get('last_name'),
+            dojo=Dojo
+            
+        )
+        return redirect('/')
+    context = {
+        "ninja":Ninja.objects.all(),
+        "dojos": Dojo.objects.all()
+    }
+    return render(request,"index.html",context)
