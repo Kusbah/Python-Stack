96b598b280fb2e61bb3be37f3fbf6776d8091d54
kusbah <abdkusbah2@gmail.com>
2025-06-17 15:55:18 +0300

    complete Semi-Restful TV Shows Validated

--- SemiRestfulTVShows/tvShow/views.py
+++ SemiRestfulTVShows/tvShow/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render ,redirect,get_object_or_404
 from .models import *
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
-        new_show = Show.objects.create(
-        title = request.POST.get('title'),
-        network = request.POST.get('network'),
-        release_date = request.POST.get('release_date'),
-        description = request.POST.get('description'),
-        )
-        return redirect('show_detail',id=new_show.id)
-    return render(request,'index.html')
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return render(request, 'index.html')
+        else:
+            new_show = Show.objects.create(
+                title=request.POST.get('title'),
+                network=request.POST.get('network'),
+                release_date=request.POST.get('release_date'),
+                description=request.POST.get('description'),
+            )
+            messages.success(request, "Show successfully created")
+            return redirect('show_detail', id=new_show.id)
+
+    return render(request, 'index.html')
 
 def show_detail(request, id):
     show = get_object_or_404(Show, id=id)
@@ -36,12 +45,17 @@ def edit_show(request, id):
     show = get_object_or_404(Show, id=id)
     
     if request.method == 'POST':
-        show.title = request.POST.get('title')
-        show.network = request.POST.get('network')
-        show.release_date = request.POST.get('release_date')
-        show.description = request.POST.get('description')
-        show.save()
-        return redirect('show_detail', id=show.id)
+        errors = Show.objects.basic_validator(request.POST)
+        for key ,value in errors.items():
+            messages.error(request,value)
+            return render(request , 'edit_show.html')
+        else:
+            show.title = request.POST.get('title')
+            show.network = request.POST.get('network')
+            show.release_date = request.POST.get('release_date')
+            show.description = request.POST.get('description')
+            show.save()
+            return redirect('show_detail', id=show.id)
     context = {
         'show' : show
     }
