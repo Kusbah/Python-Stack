6bb3a36e81ea320e70425255a502177321924abb
kusbah <abdkusbah2@gmail.com>
2025-06-19 17:46:50 +0300

    complete Favorite Books (Shell) assig

---
+++ favoriteBooks/favorite_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    uploaded_by = models.ForeignKey(User, related_name="books_uploaded", on_delete=models.CASCADE)
+    users_who_like = models.ManyToManyField(User, related_name="liked_books")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
